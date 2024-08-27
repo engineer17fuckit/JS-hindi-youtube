cf9cc6c729ea42c4b40a707fedfbd47f42972735
Rajveer_Singh07 <105161890+engineer17fuckit@users.noreply.github.com>
2024-08-27 14:47:38 +0000

    variable updated

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 145552
+let accountEmail = "thakurrajveer@hmsil.com"
+var accountPassword = "kri07shna"
+acountCity = "jaipur"
+
+acountCity = "kanker"
+console.log(accountId);
+/*
+prefer not to use var because of isssue in block slope and functional scope
+*/
+
+
+
+
+console.table([ accountId,acountCity]);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,11 @@
+    "use strict";// treat all js code as newer version
+    //alert (3+3)// we are using nodejs not browser
+
+//datatype
+
+//number=>2 to power 53
+//bigint when the number is large in quantity
+//string =>"hitesh"
+//boolean =>true/false
+//null
+//undefind
