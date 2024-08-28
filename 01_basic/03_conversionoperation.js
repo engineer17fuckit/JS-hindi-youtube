1bcc51bf37cb3553632a73a1108dc5340ecfbcca
Rajveer_Singh07 <105161890+engineer17fuckit@users.noreply.github.com>
2024-08-28 08:04:31 +0000

    conversion operation done

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,23 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(score);
+
+let valueInNumber = (score)
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+//notes
+// "33"=> 33
+//"33abc" =>NaN
+//"true" => 1
+
+let isLoggedIn = 1
+ let booleanLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanLoggedIn);
+ console.log(typeof booleanLoggedIn);
+ 
+ // "" => false
+ // "hitesh"=> true
+    
+ 
